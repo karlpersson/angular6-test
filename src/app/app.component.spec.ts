0240087d458423d904487053b191a2840a1708cf
Karl Persson <karl.persson@essvision.se>
2018-08-23 10:19:32 +0200

    Testing change detection

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { AppComponent } from './app.component';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AuthService } from './auth.service';
+import { DebugElement} from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('Hello world', () => {
   let expected = "";
   let authService: AuthService;
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +18,12 @@ describe('Hello world', () => {
     }).compileComponents();
 
       expected = "Hello World";
+
+      fixture = TestBed.createComponent(AppComponent);
+
+      authService = TestBed.get(AuthService);
+
+      el = fixture.debugElement.query(By.css('a'));
   }));
 
   afterEach(() => {
@@ -23,7 +32,6 @@ describe('Hello world', () => {
   });
 
   it('says hello', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
 
     expect(app.helloWorld()).toEqual(expected);
@@ -33,9 +41,19 @@ describe('Hello world', () => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     
-    authService = TestBed.get(AuthService);
+
     expect(authService.isAuthenticated()).toBeFalsy();
     expect(component.needsLogin()).toBeTruthy();
+  });
+
+  it('login button hidden when the user is authenticated', () => {
+      expect(el.nativeElement.textContent.trim()).toBe('');
+      fixture.detectChanges();
+      expect(el.nativeElement.textContent.trim()).toBe('Login');
+      spyOn(authService, 'isAuthenticated').and.returnValue(true);
+      expect(el.nativeElement.textContent.trim()).toBe('Login');
+      fixture.detectChanges();
+      expect(el.nativeElement.textContent.trim()).toBe('Logout');
   });
 
 
